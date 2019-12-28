39eb2fbd36fe5c4a57fe87df71f0108a2d652fa0
Michael Kim <micklepickle98@gmail.com>
2019-12-28 01:06:53 -0500

    Add browser Lyra

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use(express.static(__dirname + '/../public'));
+app.use(express.static(__dirname + '/public'));
 
 app.use('/', require('./routes'));
 
